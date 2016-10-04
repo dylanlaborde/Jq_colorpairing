9cad6a6a3acd99b4b7c0cbe25edb35b9bb982949
LABORDE <dylan4047@gmail.com>
2016-10-04 10:22:02 +0200

    initial commit

--- app.js
+++ app.js
@@ -1,1 +1,27 @@
-// good luck !
+
+
+
+
+
+var colors =$(".color");
+
+$(colors).css("background",function(){
+	var btcolors =$(this).data("color");
+	return btcolors
+
+});
+
+$(colors).click(function(){
+	var btcolors =$(this).data("color");
+	console.log(btcolors);
+	$("body").css("background", function(){
+		return btcolors;
+
+
+	});
+
+
+
+});
+
+
